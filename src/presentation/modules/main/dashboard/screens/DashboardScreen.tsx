3440b11face67854195007c665576fcd3f6444aa
frk <73194406+frankarone@users.noreply.github.com>
2026-05-24 08:53:27 -0500

    Avance dashboard principal

---
+++ src/presentation/modules/main/dashboard/screens/DashboardScreen.tsx
@@ -0,0 +1,146 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, Alert } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { useAuthStore } from '../../../auth/store/authStore';
+
+
+export default function DashboardScreen() {
+
+  // funciones
+  const registrarDano = () => {
+    Alert.alert('Registrar daño', 'Aquí irá la pantalla para registrar daños de fruta');
+  };
+
+  const verRegistros = () => {
+    Alert.alert('Ver registros', 'Aquí se mostrarán los registros guardados');
+  };
+
+  const generarReportes = () => {
+    Alert.alert('Generar reportes', 'Aquí se generará el reporte para el cliente');
+  };
+
+  const { logout } = useAuthStore();
+  const cerrarSesion = () => {
+    logout();
+  };
+
+  //frontend del dashboard
+  return (
+    <View style={styles.container}>      
+      <Text style={styles.titulo}>OPERACIONES</Text>  
+      <Text style={styles.subtitulo}>Control de daños de frutas</Text>
+
+      {/* contenedor de opciones */}
+      <View style={styles.grid}>
+
+        <TouchableOpacity style={styles.card} onPress={registrarDano}>
+          <Ionicons name="create-outline" size={35} color="#2e86de" />
+          <Text style={styles.textoCard}>Registrar daño</Text>
+        </TouchableOpacity>
+      
+        <TouchableOpacity style={styles.card} onPress={verRegistros}>
+          <Ionicons name="list-outline" size={35} color="#27ae60" />
+          <Text style={styles.textoCard}>Ver registros</Text>
+        </TouchableOpacity>
+
+  
+        <TouchableOpacity style={styles.card} onPress={generarReportes}>
+          <Ionicons name="document-text-outline" size={35} color="#f39c12" />
+          <Text style={styles.textoCard}>Generar reportes</Text>
+        </TouchableOpacity>
+
+        
+        <TouchableOpacity style={styles.card} onPress={cerrarSesion}>
+          <Ionicons name="log-out-outline" size={35} color="#e74c3c" />
+          <Text style={styles.textoCard}>Cerrar sesión</Text>
+        </TouchableOpacity>
+
+      </View>
+
+      {/*puntos*/}
+      <View style={styles.puntos}>
+        <View style={styles.puntoVacio} />
+        <View style={styles.puntoActivo} />
+        <View style={styles.puntoVacio} />
+        <View style={styles.puntoVacio} />
+        <View style={styles.puntoVacio} />
+      </View>
+
+    </View>
+  );
+}
+
+//estilos
+const styles = StyleSheet.create({
+  
+  container: {
+    flex: 1,
+    backgroundColor: '#e4dede',
+    padding: 20,
+    justifyContent: 'center',
+  },
+
+  titulo: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#222',
+    marginBottom: 5,
+  },
+
+  subtitulo: {
+    fontSize: 16,
+    textAlign: 'center',
+    color: '#555',
+    marginBottom: 30,
+  },
+
+  grid: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+  },
+
+  card: {
+    width: '47%',
+    height: 130,
+    backgroundColor: '#c0fa8a',
+    borderRadius: 10,
+    marginBottom: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 2,
+    borderColor: '#160303',
+  },
+
+  textoCard: {
+    marginTop: 10,
+    fontSize: 14,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#333',
+  },
+
+  puntos: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 25,
+  },
+
+  puntoActivo: {
+    width: 12,
+    height: 12,
+    backgroundColor: '#000',
+    borderRadius: 6,
+    marginHorizontal: 6,
+  },
+
+  puntoVacio: {
+    width: 12,
+    height: 12,
+    borderWidth: 1,
+    borderColor: '#000',
+    borderRadius: 6,
+    marginHorizontal: 6,
+  },
+});
---
+++ src/presentation/navigation/MainNavigator.tsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { View , Text , Image , StyleSheet } from 'react-native';
+import DashboardScreen from '../modules/main/dashboard/screens/DashboardScreen';
+import { Ionicons } from '@expo/vector-icons';
+import { useAuthStore } from '../modules/auth/store/authStore';
+
+export type MainStackParamList = {
+  Dashboard: undefined;
+};
+
+
+
+export function MainNavigator() {
+
+  //funciones
+  const Stack = createNativeStackNavigator<MainStackParamList>();
+  const user = useAuthStore((s) => s.user);
+
+  //froentend del navigator
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: true,
+
+        headerStyle: {
+          backgroundColor: '#f1eca2',
+        },
+        
+        headerShadowVisible: true,
+
+        headerTitle: () => (
+          <View style={styles.headerTitulo}>
+            <Image
+              source={require('../../../assets/Logo.png')}
+              style={styles.logo}
+            />
+            <Text style={styles.nombreSistema}>FPPMS</Text>
+          </View>
+
+        ),
+
+      headerRight: () => (
+        <View style={styles.usuarioCaja}>
+          <Ionicons name="person" size={24} color="#f7bfbf" />
+          <Text style={styles.usuarioTexto}>{user?.name}</Text>
+          </View>
+        ),
+      }}
+    >
+
+      <Stack.Screen name="Dashboard" component={DashboardScreen}/>
+    </Stack.Navigator>
+  );
+}
+
+//estilos
+const styles = StyleSheet.create({
+  headerTitulo: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+
+  logo: {
+    width: 85,
+    height: 35,
+    resizeMode: 'contain',
+    marginRight: 10,
+  },
+
+  nombreSistema: {
+    fontSize: 17,
+    color: '#777',
+  },
+
+  usuarioCaja: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#3da87c',
+    paddingHorizontal: 8,
+    paddingVertical: 5,
+    borderRadius: 3,
+  },
+
+  usuarioTexto: {
+    marginLeft: 4,
+    fontSize: 13,
+    color: '#555',
+  },
+});
