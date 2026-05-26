4321dc80ee2326c712cf15c71c46d893c6d20f0b
jsalazarat <joseph.salazar.ext@makers150.com>
2026-05-26 18:00:22 -0500

    Refactor App component to remove unused navigators and screens

--- App.js
+++ App.js
@@ -1,17 +1,7 @@
 import React, { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { AppNavigator } from './src/presentation/navigation/AppNavigator';
-import { MainNavigator } from './src/presentation/navigation/MainNavigator';
-import { AuthNavigator } from './src/presentation/navigation/AuthNavigator';
 import { useAuthStore } from './src/presentation/modules/auth/store/authStore';
-
-import { DamageListScreen } from './src/presentation/modules/main/catalog/screens/DamageListScreen'; //Pantalla del lista de registros
-import { DamageDetailScreen } from './src/presentation/modules/main/catalog/screens/DamageDetailScreen';
-
-import { ForgotPasswordScreen } from './src/presentation/modules/auth/screens/ForgotPasswordScreen'; //Recuperacion de contraseña
-import { LoginScreen } from './src/presentation/modules/auth/screens/LoginScreen'; //Inicio de sesion
-import { RegisterScreen } from './src/presentation/modules/auth/screens/RegisterScreen'; //Registro de usuario
-import { UpdatePasswordScreen } from './src/presentation/modules/auth/screens/UpdatePasswordScreen'; //Actualizacion de contraseña
 
 export default function App() {
   const checkSession = useAuthStore((s) => s.checkSession);
@@ -22,7 +12,7 @@ export default function App() {
 
   return (
     <>
-      <  DamageDetailScreen />
+      <AppNavigator />
       <StatusBar style="auto" />
     </>
   );
